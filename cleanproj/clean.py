dc7942113e7b5a286e20715db8b9b8b46d943f44
Goclis Yao <goclisyyh@gmail.com>
2015-03-07 15:37:16 +0800

    【cleanproj】增加删除指定文件名的功能

--- cleanproj/clean.py
+++ cleanproj/clean.py
@@ -2,17 +2,25 @@
 # -*- coding:utf-8 -*-
 
 import os
-import os.path
 import shutil
 
 
+# Remove directories and files
 # 删除rootdir下的名字在dirs_to_delete中的文件夹
-def rmdir(rootdir, dirs_to_delete):
+# 删除rootdir下名字为files_to_delete中的文件
+def rmdfs(rootdir, dirs_to_delete, files_to_delete = []):
 	for (dirpath, subdirs, filenames) in os.walk(rootdir):
+		for f in filenames:
+			if f in files_to_delete:
+				path = os.path.join(dirpath, f)
+				os.remove(path)
+				print 'File %s deleted.' % path
+
 		for subdir in subdirs:
 			if subdir in dirs_to_delete:
-				print 'Delete dir %s' % os.path.join(dirpath, subdir)
-				shutil.rmtree(os.path.join(dirpath, subdir))
+				path = os.path.join(dirpath, subdir)
+				shutil.rmtree(path)
+				print 'Directory %s deleted.' % path
 
 		subdirs[:] = [d for d in subdirs if d not in dirs_to_delete]
 
@@ -20,7 +28,13 @@ def rmdir(rootdir, dirs_to_delete):
 def main():
 	cwd = os.getcwd()
 	dirs = ['Debug', 'Release', 'ipch']
-	rmdir(cwd, dirs)
+	files = ['zrddsgen.sdf']
+	try:
+		rmdfs(cwd, dirs, files)
+	except:
+		print u'Error: Please try again after closing the Visual Studio.'
+
+	os.system("pause")
 
 
 if __name__ == '__main__':
